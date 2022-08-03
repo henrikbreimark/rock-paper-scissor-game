fb593b0ae51df9a7267f93a9be682f2af11e8a53
Henrik Breimark <henrik.breimark@gmail.com>
2022-08-03 22:25:18 +0200

    Adds a function that generates rock or paper or scissor at random.

--- game-logic.js
+++ game-logic.js
@@ -1,1 +1,12 @@
-
+function getComputerChoice () {
+    let computerChoice;
+    let randomiser = Math.floor(Math.random()*3)+1;
+    if (randomiser===1){
+        computerChoice= "Scissors"
+    } else if (randomiser===2){
+        computerChoice = "Rock"
+    } else if (randomiser===3){
+        computerChoice = "Paper"
+}
+return computerChoice;
+}
